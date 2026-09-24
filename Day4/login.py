8fcd70549c79d99cd2ad85b3b4c41a6964d6b028
PariJain18 <parijain0087@gmail.com>
2026-09-24 22:25:29 +0530

    Day 4 completed

---
+++ Day4/login.py
@@ -0,0 +1,8 @@
+username=input("Enter username:")
+password=input("Enter password")
+if username=="pari" and password=="1850":
+    print("login successful:")    
+else:
+    print("Invalid username or password")    
+
+        
